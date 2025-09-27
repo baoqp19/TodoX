6df6f2da3113645ad0a48d4f5312295b6fc81746
= <minhducdtr70@gmail.com>
2025-09-27 21:21:29 +0700

    build app

--- READMY.md
+++ READMY.md
@@ -1,2 +1,6 @@
 -D: devDevolopment 
 type: "module" thêm vào khi tạo package.json npm init -y 
+
+
+npm install --prefix backend && npm install --prefix frontend && npm run build --prefix frontend"
+=> cài node_module cho frontend vs backend và npm run build để tối ưu frontend và tạo thư mục dist
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,30 @@ import taskRoute from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 // middlewares
 app.use(express.json()); // middleware để parse json
-app.use(cors({ origin: "http://localhost:5173" })); // cho phép truy cập từ frontend
 
+if (process.env.NODE_ENV !== "development") {
+  app.use(cors({ origin: "http://localhost:5173" })); // cho phép truy cập từ frontend{
+}
 app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
